17b692fa1bac3805723d2d0dda25e28cb26776cb
Guilherme Rocha <guilhermechaves2005@hotmail.com>
2024-01-25 00:59:13 +0000

    feat: add meus-projetos route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={roboto.className}>
-        {/* <Header /> */}
+        <Header />
         {children}
       </body>
     </html>
---
+++ src/app/meus-projetos/page.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import ProfilePicture from '../../../public/assets/profile-picture.png'
+
+export default function MeusProjetos() {
+    return (
+        <>
+            <section className="my-profile">
+                <Image
+                    src={ProfilePicture}
+                    alt="Profile Picture"
+                    width={150}
+                    priority
+                />
+                <h2>Camila Soares</h2>
+                <p>Brasil</p>
+                <button>Adicionar Projeto</button>
+            </section>
+
+            <section className="my-projects">
+                <h2>Meus projetos</h2>
+                <input type="text" placeholder="Buscar tags" />
+                <div className="add-your-first-project">
+                    <Image src="#" alt="Image Icon" width={60} priority />
+                    <h3>Adicione seu primeiro projeto</h3>
+                    <p>Compartilhe seu talento com milhares de pessoas</p>
+                </div>
+            </section>
+        </>
+    )
+}
