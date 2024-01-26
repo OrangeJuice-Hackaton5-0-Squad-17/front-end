6ccfa46933247b233aaea8a6975831ee0cdb3725
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-01-26 19:45:01 -0300

    refactor: sign up page with material ui components

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,9 +1,24 @@
 'use client'
 
-import { IoEyeSharp } from 'react-icons/io5'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import Image from 'next/image'
+import {
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+  Button,
+  InputAdornment,
+  IconButton,
+  Box,
+} from '@mui/material'
+import Visibility from '@mui/icons-material/Visibility'
+import VisibilityOff from '@mui/icons-material/VisibilityOff'
+
+import backgroundImg from '@/assets/images/background-sign-up.svg'
 
 const newAccountFormValidationSchema = zod.object({
   name: zod.string().min(3),
@@ -30,6 +45,16 @@ interface Account {
 }
 
 export default function SignUp() {
+  const [showPassword, setShowPassword] = useState(false)
+
+  function handleClickShowPassword() {
+    setShowPassword((show) => !show)
+  }
+
+  function handleMouseDownPassword(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault()
+  }
+
   const { register, handleSubmit, formState, reset } =
     useForm<NewAccountFormData>({
       resolver: zodResolver(newAccountFormValidationSchema),
@@ -58,52 +83,75 @@ export default function SignUp() {
 
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <section className="p-6 w-full">
-        <h1 className="text-center text-[#224] text-2xl mb-8">Cadastre-se</h1>
-        <form
-          onSubmit={handleSubmit(handleCreateNewAccount)}
-          className="flex flex-col gap-4"
-        >
-          <div>
-            <input
-              className="border rounded-md border-[#0000003a] p-3 w-full"
+      <aside className="max-w-[549px]">
+        <Image src={backgroundImg} alt="sign up background image" priority />
+      </aside>
+      <section className="flex flex-1 flex-col items-center justify-center">
+        <div className="w-[517px]">
+          <h1 className="text-center text-5xl text-[#222244 leading-10 mb-8">
+            Cadastre-se
+          </h1>
+          <form
+            onSubmit={handleSubmit(handleCreateNewAccount)}
+            className="flex flex-col gap-4"
+          >
+            <Box className="flex items-center justify-between">
+              <TextField
+                type="text"
+                id="outlined-basic"
+                label="Nome *"
+                variant="outlined"
+                {...register('name')}
+              />
+              <TextField
+                type="text"
+                id="outlined-basic"
+                label="Sobrenome *"
+                variant="outlined"
+                {...register('surname')}
+              />
+            </Box>
+            <TextField
               type="text"
-              placeholder="Nome *"
-              {...register('name')}
-            />
-          </div>
-          <div>
-            <input
-              className="border rounded-md border-[#0000003a] p-3 w-full"
-              type="text"
-              placeholder="Sobrenome *"
-              {...register('surname')}
-            />
-          </div>
-          <div>
-            <input
-              className="border rounded-md border-[#0000003a] p-3 w-full"
-              type="text"
-              placeholder="Email address *"
+              id="outlined-basic"
+              label="Email address"
+              variant="outlined"
               {...register('email')}
             />
-          </div>
-          <div className="relative">
-            <input
-              className="border rounded-md border-[#0000003a] p-3 w-full"
-              type="password"
-              placeholder="Password *"
-              {...register('password')}
-            />
-            <IoEyeSharp className="text-[#0000008A] text-2xl absolute top-1/2 right-3 -translate-y-1/2" />
-          </div>
-          <button
-            type="submit"
-            className="bg-[#f52] shadow shadow-[#00000033] rounded-md text-white uppercase p-2 w-full"
-          >
-            Cadastrar
-          </button>
-        </form>
+            <FormControl variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+                {...register('password')}
+              />
+            </FormControl>
+            <Button
+              type="submit"
+              className="w-full h-10 text-white text-sm font-medium tracking-wide uppercase mt-4 bg-[#ff5522] hover:bg-[#cc4400]"
+              variant="contained"
+              color="inherit"
+              size="large"
+            >
+              Cadastrar
+            </Button>
+          </form>
+        </div>
       </section>
     </main>
   )
