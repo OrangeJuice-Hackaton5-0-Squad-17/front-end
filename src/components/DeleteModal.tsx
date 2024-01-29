c09b4cb76adf68187fd2d94f3c4d01464858687c
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-01-29 18:12:12 -0300

    chore: create delete modal component

---
+++ src/components/DeleteModal.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import { useState } from 'react'
+import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
+import { FaTrashAlt } from 'react-icons/fa'
+
+export function DeleteModal() {
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <div className="min-h-screen">
+      <Button variant="outlined" onClick={handleOpen}>
+        <FaTrashAlt />
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle className="text-2xl text-[#515255]">
+          Deseja Excluir?
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <div>
+              <h1 className="text-base text-[#515255]">
+                Se você prosseguir irá excluir o projeto do {'\n'}seu portfólio
+              </h1>
+            </div>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            className="h-[42px] w-[106px] bg-[#ff5522] hover:bg-[#cc4400] text-white text-base font-semibold uppercase"
+            onClick={handleClose}
+          >
+            Excluir
+          </Button>
+          <Button
+            className="h-[42px] w-[124px] bg-[#dddee0] hover:bg-[#d3d3d3] text-[#818388] text-base font-semibold uppercase"
+            onClick={handleClose}
+            autoFocus
+          >
+            Cancelar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
+}
