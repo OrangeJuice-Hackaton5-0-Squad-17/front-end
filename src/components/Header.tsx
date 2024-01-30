0414e6b7f9a7de55510b7f3d51779546a8f1c2f6
Guilherme Rocha <guilhermechaves2005@hotmail.com>
2024-01-30 03:32:09 +0000

    feat: add responsive menu and its functions from mui lib

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,42 +1,150 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
+
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Menu from '@mui/material/Menu'
+import MenuIcon from '@mui/icons-material/Menu'
+import Container from '@mui/material/Container'
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import Tooltip from '@mui/material/Tooltip'
+import MenuItem from '@mui/material/MenuItem'
 
 import OrangeLogo from '@/assets/images/logo.svg'
 import ProfilePicture from '../../public/assets/profile-picture.png'
 import BellIcon from '../../public/assets/bell-icon.png'
 
+const pages = ['Meus projetos', 'Descobrir']
+
 export function Header() {
-  return (
-    <header className="bg-[#111133] py-4 px-8 text-white flex md:gap-24 items-center">
-      <Image src={OrangeLogo} alt="Orange Logo" width={110} priority />
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+        null,
+    )
 
-      <nav className="hidden md:block">
-        <ul className="flex gap-6 list-none">
-          <li>
-            <Link href="/meus-projetos">Meus projetos</Link>
-          </li>
-          <li>
-            <Link href="/descobrir">Descobrir</Link>
-          </li>
-        </ul>
-      </nav>
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget)
+    }
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget)
+    }
 
-      <div className="flex items-center gap-4 ml-auto">
-        <Image
-          className="rounded-full"
-          src={ProfilePicture}
-          alt="Profile Picture"
-          width={40}
-          priority
-        />
-        <Image
-          className="w-4"
-          src={BellIcon}
-          alt="Bell Icon"
-          width={15}
-          priority
-        />
-      </div>
-    </header>
-  )
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null)
+    }
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null)
+    }
+
+    return (
+        <header className="bg-[#111133] py-4 pl-6 pr-8 text-white flex md:gap-24 items-center">
+            <Toolbar disableGutters className="w-full">
+                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleOpenNavMenu}
+                        color="inherit"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorElNav}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                        open={Boolean(anchorElNav)}
+                        onClose={handleCloseNavMenu}
+                        sx={{
+                            display: { xs: 'block', md: 'none' },
+                        }}
+                    >
+                        {pages.map((page) => (
+                            <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                <Typography textAlign="center">{page}</Typography>
+                            </MenuItem>
+                        ))}
+                    </Menu>
+
+                    <Image
+                        src={OrangeLogo}
+                        alt="Orange Portfolio Logo"
+                        width={110}
+                        priority
+                    />
+                </Box>
+
+                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                    {pages.map((page) => (
+                        <Button
+                            key={page}
+                            onClick={handleCloseNavMenu}
+                            sx={{ my: 2, color: 'white', display: 'block' }}
+                        >
+                            {page}
+                        </Button>
+                    ))}
+                </Box>
+
+                <div className="flex items-center gap-4 ml-auto">
+                    <Tooltip title="Open settings">
+                        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                            <Image
+                                className="rounded-full"
+                                alt="User Profile Picture"
+                                src={ProfilePicture}
+                                width={40}
+                                priority
+                            />
+                        </IconButton>
+                    </Tooltip>
+                    <Menu
+                        sx={{ mt: '45px' }}
+                        id="menu-appbar"
+                        anchorEl={anchorElUser}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        open={Boolean(anchorElUser)}
+                        onClose={handleCloseUserMenu}
+                    >
+                        <MenuItem onClick={handleCloseUserMenu}>
+                            <Typography textAlign="center">Logout</Typography>
+                        </MenuItem>
+                    </Menu>
+                </div>
+
+                <Image
+                    className="w-4 ml-4"
+                    src={BellIcon}
+                    alt="Bell Icon"
+                    width={15}
+                    priority
+                />
+            </Toolbar>
+        </header>
+    )
 }
