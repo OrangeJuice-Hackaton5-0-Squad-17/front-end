37ca8c3978252a184e2cf0715c64d2ad5a056835
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-02-04 20:32:19 -0300

    refactor: add file that contains mock users data constant

---
+++ src/lib/constants.ts
@@ -0,0 +1,39 @@
+import backgroundProjectDiscoverFirst from '@/assets/images/background-project-discover-first.svg'
+import backgroundProjectDiscoverSecond from '@/assets/images/background-project-discover-second.svg'
+import backgroundProjectDiscoverThird from '@/assets/images/background-project-discover-third.svg'
+import backgroundProjectDiscoverFourth from '@/assets/images/background-project-discover-fourth.svg'
+import profileImg from '@/assets/images/default-profile-picture.svg'
+
+export const mockUsersData = [
+  {
+    name: 'Bianca Martin',
+    avatar: profileImg,
+    date: '02/24',
+    tags: ['UX', 'Web'],
+    image: backgroundProjectDiscoverFirst,
+  },
+  {
+    name: 'José Lima',
+    avatar:
+      'https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light',
+    date: '01/24',
+    tags: ['Front-end', 'Web'],
+    image: backgroundProjectDiscoverSecond,
+  },
+  {
+    name: 'Ana Beatriz',
+    avatar:
+      'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairDreads01&accessoriesType=Kurt&hairColor=BrownDark&facialHairType=BeardLight&facialHairColor=BrownDark&clotheType=ShirtCrewNeck&clotheColor=Gray01&eyeType=Squint&eyebrowType=SadConcernedNatural&mouthType=Sad&skinColor=Tanned',
+    date: '05/23',
+    tags: ['Back-end', 'Mobile'],
+    image: backgroundProjectDiscoverThird,
+  },
+  {
+    name: 'Bianca Martin',
+    avatar:
+      'https://avataaars.io/?avatarStyle=Circle&topType=LongHairDreads&accessoriesType=Round&hairColor=BlondeGolden&facialHairType=BeardMajestic&facialHairColor=Blonde&clotheType=ShirtVNeck&clotheColor=PastelYellow&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Sad&skinColor=Light',
+    date: '03/23',
+    tags: ['Full-stack', 'DevOps'],
+    image: backgroundProjectDiscoverFourth,
+  },
+]
