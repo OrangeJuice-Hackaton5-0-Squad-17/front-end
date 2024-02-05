aaf6d2ef3beb3d80ffb06ff09a97b87a0aedc600
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-02-04 22:11:25 -0300

    refactor(imagedropzone): add functionality that enables user to see image preview

--- src/components/ImageDropzone.tsx
+++ src/components/ImageDropzone.tsx
@@ -1,6 +1,9 @@
-import { useCallback } from 'react'
+'use client'
+
+import { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import Image from 'next/image'
+import { Box, Paper, Typography } from '@mui/material'
 
 import dropzoneIcon from '@/assets/images/dropzone-icon.svg'
 
@@ -9,16 +12,28 @@ interface ImageDropzoneProps {
 }
 
 export function ImageDropzone({ onDrop }: ImageDropzoneProps) {
+  const [imagePreview, setImagePreview] = useState<string | null>(null)
+
   const handleDrop = useCallback(
     (acceptedFiles: File[]) => {
       if (acceptedFiles.length > 0) {
         onDrop(acceptedFiles[0])
       }
+
+      const file = acceptedFiles
+
+      const reader = new FileReader()
+
+      reader.onload = () => {
+        if (typeof reader.result === 'string') setImagePreview(reader.result)
+      }
+
+      reader.readAsDataURL(file[0])
     },
     [onDrop],
   )
 
-  const { getRootProps, getInputProps } = useDropzone({
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: {
       'image/png': ['.png'],
       'image/jpg': ['.jpg'],
@@ -29,19 +44,43 @@ export function ImageDropzone({ onDrop }: ImageDropzoneProps) {
   })
 
   return (
-    <div {...getRootProps()} className="dropzone">
-      <p className="text-base text-[#515255] mb-4">
+    <Paper
+      {...getRootProps()}
+      className={`dropzone shadow-none ${isDragActive ? 'bg-gray-200' : ''}`}
+    >
+      <Typography className="text-base text-[#515255] mb-4">
         Selecione o conteúdo que você deseja fazer upload
-      </p>
-      <div className="w-full h-[304px] bg-[#e6e9f2] rounded cursor-pointer">
-        <div className="w-full h-full flex flex-col items-center justify-center">
+      </Typography>
+      <Box className="w-full h-[304px] bg-[#e6e9f2] rounded cursor-pointer shadow-lg">
+        <Box className="w-full h-full flex flex-col items-center justify-center">
           <input className="w-full h-full" {...getInputProps()} />
-          <Image src={dropzoneIcon} alt="dropzone icon" width={46} priority />
-          <span className="text-base text-center text-[#303133] mt-4">
-            Compartilhe seu talento com milhares de pessoas
-          </span>
-        </div>
-      </div>
-    </div>
+          {imagePreview ? (
+            <Box className="w-full h-full relative">
+              <Image
+                src={imagePreview}
+                alt="preview image"
+                layout="fill"
+                objectFit="contain"
+                priority
+              />
+            </Box>
+          ) : (
+            <>
+              <Image
+                src={dropzoneIcon}
+                alt="dropzone icon"
+                width={46}
+                priority
+              />
+              <Typography className="text-base text-center text-[#303133] mt-4">
+                {isDragActive
+                  ? 'Solte a imagem aqui'
+                  : 'Compartilhe seu talento com milhares de pessoas'}
+              </Typography>
+            </>
+          )}
+        </Box>
+      </Box>
+    </Paper>
   )
 }
